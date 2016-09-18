3e217ca25896bc522fb34eb4a1639a08f307df9b
Ad5001 <ad5001yt@gmail.com>
2016-09-18 13:05:32 +0300

    Whoohoo ! Working !

--- ImagicalPlugCreator/js/main.js
+++ ImagicalPlugCreator/js/main.js
@@ -44,6 +44,8 @@
 
         var submit = document.getElementById("submit");
         submit.addEventListener("click", generatePlugin, false);
+        var ChooseFolder = document.getElementById("chooseFolder");
+        ChooseFolder.addEventListener("click", chooseFolder, false);
     };
 
     function onVisibilityChanged(args) {
@@ -58,14 +60,14 @@
         // Si vous devez effectuer une opération asynchrone avant la suspension de votre application, appelez args.setPromise().
     };
     function define() {
-        var mainphp = ['<?php', "\n",
+        mainphp = ['<?php', "\n",
             "namespace {author}\\{plugin};", "\n",
-            'use pocketmine\command\CommandSender;', "\n",
-            'use pocketmine\command\Command;', "\n",
-            'use pocketmine\event\Listener;', "\n",
-            'use pocketmine\plugin\PluginBase;', "\n",
-            'use pocketmine\Server;', "\n",
-            'use pocketmine\Player;', "\n",
+            'use pocketmine\\command\\CommandSender;', "\n",
+            'use pocketmine\\command\\Command;', "\n",
+            'use pocketmine\\event\\Listener;', "\n",
+            'use pocketmine\\plugin\\PluginBase;', "\n",
+            'use pocketmine\\Server;', "\n",
+            'use pocketmine\\Player;', "\n",
            "\n", "\n",
             'class Main extends PluginBase{', "\n\n\n",
             "   public function onEnable(){", "\n",
@@ -84,11 +86,11 @@
             '    }', "\n",
             '}'];
 
-        var taskphp = ['<?php', "\n",
+        taskphp = ['<?php', "\n",
             "namespace {author}\\{plugin}\\tasks;", "\n\n",
-            'use pocketmine\Server;', "\n",
-            'use pocketmine\scheduler\PluginTask;', "\n",
-            'use pocketmine\Player;', "\n\n",
+            'use pocketmine\\Server;', "\n",
+            'use pocketmine\\scheduler\PluginTask;', "\n",
+            'use pocketmine\\Player;', "\n\n",
             "use {author}\\{plugin}\\Main;", "\n\n",
            "\n", "\n",
             'class Task{id} extends PluginTask {', "\n\n\n",
@@ -102,7 +104,7 @@
             '    }', "\n\n\n",
             '}'];
 
-        var pluginyml = ["---",
+        pluginyml = ["---",
             "name: {plugin}",
             "author:  {author}",
             "version: 1.0",
@@ -111,22 +113,56 @@
             "commands: []",
             "permissions: []",
         "..."];
+        dummyphp = ['<?php',  "\n",
+		"namespace {author}\\{name};", "\n\n",
+        'use pocketmine\\Server;', "\n",
+        'use pocketmine\\Player;', "\n\n",
+        "use {author}\\{name}\\Main;", "\n\n",
+	   "\n", "\n",
+        'class Dummy {', "\n\n\n",
+        '   public function __construct(Main $main) {', "\n",
+	    '        $this->main = $main;', "\n",
+        '        $this->server = $main->getServer()', "\n",
+        '    }', "\n\n\n",
+	    '}'];
     }
 
     define();
 
-    function saveData (blob, fileName) {
-        var a = document.getElementById("dl");
-        url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = fileName;
-        a.click();
-        window.URL.revokeObjectURL(url);
-        console.log("Downloaded !")
-    };
-
     function generatorPlugin(event) {
-        console.log("Hey");
+        WinJS.log && WinJS.log("Hey", "sample", "status");
+    }
+
+    function chooseFolder(event) {/*
+        chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setDialogTitle("select folder");
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);*/
+        // Verify that we are currently not snapped, or that we can unsnap to open the picker
+        var currentState = Windows.UI.ViewManagement.ApplicationView.value;
+        if (currentState === Windows.UI.ViewManagement.ApplicationViewState.snapped &&
+            !Windows.UI.ViewManagement.ApplicationView.tryUnsnap()) {
+            // Fail silently if we can't unsnap
+            return;
+        }
+
+        // Create the picker object and set options
+        var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+        folderPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.desktop;
+        // Users expect to have a filtered view of their folders depending on the scenario.
+        // For example, when choosing a documents folder, restrict the filetypes to documents for your application.
+        folderPicker.fileTypeFilter.replaceAll([".docx", ".xlsx", ".pptx"]);
+
+        folderPicker.pickSingleFolderAsync().then(function (folder) {
+            if (folder) {
+                // Application now has read/write access to all contents in the picked folder (including sub-folder contents)
+                // Cache folder so the contents can be accessed at a later time
+                Windows.Storage.AccessCache.StorageApplicationPermissions.futureAccessList.addOrReplace("PickedFolderToken", folder);
+                document.getElementById("choosed").innerHTML = folder.path;
+                document.getElementById("choosedP").innerHTML = "Picked folder: " + folder.name;
+            }
+        });
     }
 
     function generatePlugin(event) {
@@ -134,54 +170,152 @@
         var author = document.getElementById("author").value;
         var config = document.getElementById("config").checked;
         var tasks = document.getElementById("tasks").value;
-        var dummies = document.getElementById("number").value;
-        var events = documents.getElementById("events").checked;
-        zip.createWriter(new zip.BlobWriter(), function (writer) {
+        var dummies = document.getElementById("dummies").value;
+        var events = document.getElementById("events").checked;
+        var path = document.getElementById("choosed").innerHTML;
+        var f = false;
+        doneDummies = false;
+        doneTasks = false;
+        if (name !== "" && author !== "" && tasks !== "" && dummies !== "" && path !== "" && document.getElementById("name").checkValidity() && document.getElementById("author").checkValidity() && document.getElementById("tasks").checkValidity() && document.getElementById("dummies").checkValidity()) {
+            document.getElementById("loading").style.opacity = 1;
+            Windows.Storage.StorageFolder.getFolderFromPathAsync(path).done(function (f) {
+                if (f) {
+                    f.tryGetItemAsync(name).done(
+                    function (found) {
+                        if (found == null) {
+                            f.createFolderAsync(name).done(function (f) {
+                                if (f) {
+                                    if (config || config == "true") {
+                                        f.createFolderAsync("resources").done(function (folder) {
+                                            if (folder) {
+                                                filePutContents(folder, "config.yml", "---\n# This is the default config generated with ImagicalPlugCreator. (C) ImagicalPlugCreator - Ad5001 2016\n...");
+                                            } else {
+                                                document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Failed to create plugin resources folder !<br />";
+                                            }
+                                        });
+                                    } else {
+                                        delete mainphp[20];
+                                        delete mainphp[21];
+                                        delete mainphp[26];
+                                        delete mainphp[27];
+                                        delete mainphp[28];
+                                        delete mainphp[29];
+                                        delete mainphp[30];
+                                        delete mainphp[31];
+                                    }
 
-            if (!config || config == "false") {
-                delete mainphp[20];
-                delete mainphp[21];
-                delete mainphp[26];
-                delete mainphp[27];
-                delete mainphp[28];
-                delete mainphp[29];
-                delete mainphp[30];
-                delete mainphp[31];
-            }
 
-            if (!events || events == "false") {
-                delete mainphp[22];
-                delete mainphp[23];
-            } else {
-                mainphp[16] = "class Main extends PluginBase implements Listener {";
-            }
-            writer.add("src\\" + author + "\\" + name + "\\Main.php", new zip.TextReader(mainphp.replace("{author}", author).replace("{plugin}", name)), function () { },
-                function (currentIndex, totalIndex) {
-                    document.getElementById("status").innerHTML = "Processing src\\" + author + "\\" + name + "\\Main.php. " + currentIndex / totalIndex * 100 + "%.";
-                });
 
-            for (i = 0; i <= tasks; i++) {
-                writer.add("src\\" + author + "\\" + name + "\\tasks\\" + i + ".php", new zip.TextReader(taskphp.replace("{author}", author).replace("{plugin}", name).replace("{id}", i)), function () { },
-                    function (currentIndex, totalIndex) {
-                        document.getElementById("status").innerHTML = "Processing src\\" + author + "\\" + name + "\\Task" + i + ".php. " + currentIndex / totalIndex * 100 + "%.";
+                                    if (!events || events == "false") {
+                                        delete mainphp[22];
+                                        delete mainphp[23];
+                                    } else {
+                                        mainphp[18] = "class Main extends PluginBase implements Listener {";
+                                    }
+
+
+
+                                    document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Generating folders...<br />";
+                                    f.createFolderAsync("src").done(function (src) { // src folder
+                                        if (src) {
+                                            src.createFolderAsync(author).done(function (src) { // src/author folder
+                                                if (src) {
+                                                    src.createFolderAsync(name).done(function (src) {
+                                                        if (src) {
+                                                            filePutContents(src, "Main.php", mainphp.join("\n").replace("{author}", author).replace("{plugin}", name));
+
+                                                            if (tasks > 0) {
+                                                                document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Creating folder src\\" + author + "\\" + name + "\\tasks...<br />";
+                                                                src.createFolderAsync("tasks").done(function (folder) {
+                                                                    if (folder) {
+                                                                        for (i = 1; i <= tasks; i++) {
+                                                                            filePutContents(folder, "Task" + i + ".php", taskphp.join("\n").replace("{author}", author).replace("{plugin}", name).replace("{id}", i));
+                                                                        }
+                                                                    } else {
+                                                                        document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Failed to create plugin src/" + author + "/" + name + "/tasks folder !<br />";
+                                                                    }
+                                                                });
+                                                                for (i = 1; i <= dummies; i++) {
+                                                                    filePutContents(src, "Dummy" + i + ".php", dummyphp.join("\n").replace("{author}", author).replace("{plugin}", name).replace("{id}", i));
+                                                                }
+
+                                                                filePutContents(f, "plugin.yml", pluginyml.join("\n").replace("{author}", author).replace("{plugin}", name));
+                                                                document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Succefully created plugin base " + name + " !<br />";
+                                                                define();
+                                                            } else {
+                                                                document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Failed to create plugin src/" + author + "/" + name + " folder !<br />";
+                                                            }
+                                                        }
+                                                    });
+                                                } else {
+                                                    document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Failed to create plugin src/" + author + " folder !<br />";
+                                                }
+                                            });
+
+                                        } else {
+                                            document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Failed to create plugin src folder !<br />";
+                                        }
+                                    });
+                                } else {
+                                    document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Failed to create plugin folder !<br />";
+                                }
+                            });
+                        } else {
+                            document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Plugin with name " + name + " already exists !<br />";
+                        }
                     });
+                } else {
+                    document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "There were an error about the saving folder !<br />";
+                }
             }
-            for (i = 0; i <= dummies; i++) {
-                writer.add("src\\" + author + "\\" + name + "\\Dummy" + i + ".php", new zip.TextReader(mainphp.replace("{author}", author).replace("{plugin}", name).replace("{id}", i)),
-                    function (currentIndex, totalIndex) {
-                        document.getElementById("status").innerHTML = "Processing src\\" + author + "\\" + name + "\\Dummy" + i + ".php. " + currentIndex / totalIndex * 100 + "%.";
-                    });
-            }
+                );
+            document.getElementById("loading").style.opacity = 0;
+        } else {
+            document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Please fill all the forms correctly !<br />";
+        }
 
-            writer.add("plugin.yml", new zip.TextReader(pluginyml.replace("{author}", author).replace("{plugin}", name)), function () {
-                writer.close(function (blob) {
-                    saveData(blob, name);
-
-                });
-            });
-            define();
-        });
     }
 
+    function filePutContents(folder, filename, contents) {
+        document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Generating file " + filename + "...<br />";
+        folder.createFileAsync(filename).done(function (file) {
+            if (file) {
+                file.openTransactedWriteAsync().then(
+                function (transaction) {
+                    var dataWriter = new Windows.Storage.Streams.DataWriter(transaction.stream);
+                    dataWriter.writeString(contents);
+                    dataWriter.storeAsync().then(function (size) {
+                        transaction.stream.size = size; // reset stream size to override the file
+                        transaction.commitAsync().done(function () {
+                            // Text written to file
+                            document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Succefully created file " + filename + " !<br />";
+                            if (filename == "Dummy" + document.getElementById("dummies").value) {
+                                if (doneTasks) {
+                                    finish();
+                                } else {
+                                    doneDummies = true;
+                                }
+                            }
+                            if (filename == "Task" + document.getElementById("tasks").value) {
+                                if (doneDummies) {
+                                    finish();
+                                } else {
+                                    doneTasks = true;
+                                }
+                            }
+                            // Close stream
+                            transaction.close();
+                        });
+                    });
+                },
+                // Handle errors with an error function
+                function (error) {
+                    document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Error ! " + name + "<br />";
+                });
+            } else {
+                document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + "Error ! Could not create file " + filename + ".<br />";
+            }
+        });
+    }
     app.start();
 }());
